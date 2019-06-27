808a0ade5875d2186b0e18eea3749b87507d7ec0
annacv <21217131+annacv@users.noreply.github.com>
2019-06-27 15:27:53 +0200

    [WIP] I2 > I3 > I5 Solved

--- src/movies.js
+++ src/movies.js
@@ -20,14 +20,39 @@ function ratesAverage(arr) {
  
 // Get the average of Drama Movies
 
+function dramaMoviesRate(arr) {
+  var filter = arr.filter(function(movie) {
+    return movie.genre.includes('Drama');
+  });
+  if (filter.length > 0 ) return ratesAverage(filter);
+}
 
 // Order by time duration, in growing order
 
 
 // How many movies did STEVEN SPIELBERG
 
+function howManyMovies(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  var counter = 0;
+
+  arr.forEach(function(movie) {
+    if (movie.genre.includes('Drama') && movie.director === 'Steven Spielberg') {
+      counter++;
+    }
+  })
+  return 'Steven Spielberg directed ' + counter + ' drama movies!';
+}
+
 
 // Order by title and print the first 20 titles
-
+/***WIP 
+function orderAlphabetically (array) {
+  var counter = 0
+  if(counter<20)array.;
+  counter++
+}*/
 
 // Best yearly rate average
